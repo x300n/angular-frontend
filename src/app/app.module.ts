5ee4d3620bc9da3452c1597b78fd99cd31e38b5a
x300n <houseofxeon@gmail.com>
2020-07-22 03:01:33 +0200

    Initialize

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductService } from './services/product.service';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { Routes, RouterModule, Router } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+
+const routes: Routes = [
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'search/:keyword', component: ProductListComponent },
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' },
+];
+
+@NgModule({
+  declarations: [AppComponent, ProductListComponent, ProductCategoryMenuComponent, SearchComponent, ProductDetailsComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, RouterModule.forRoot(routes), NgbModule],
+  providers: [ProductService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
